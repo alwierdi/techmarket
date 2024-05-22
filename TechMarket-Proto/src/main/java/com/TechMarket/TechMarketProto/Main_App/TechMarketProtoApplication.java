96998aff930c12b217f1349f50165313e7a27e49
AzkaDevanda <azkadevanda8@gmail.com>
2024-05-22 16:55:24 +0700

    nyalain backend di file Main_App

--- TechMarket-Proto/src/main/java/com/TechMarket/TechMarketProto/Main_App/TechMarketProtoApplication.java
+++ TechMarket-Proto/src/main/java/com/TechMarket/TechMarketProto/Main_App/TechMarketProtoApplication.java
@@ -1,4 +1,4 @@
-package com.TechMarket.TechMarketProto;
+package com.TechMarket.TechMarketProto.Main_App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
