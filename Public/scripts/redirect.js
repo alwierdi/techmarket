49e0c20ed2dae94f4b522cbdd9e0d862b94a2106
alwierdi <alwi.erdi4@gmail.com>
2024-05-26 21:24:19 +0700

    fix bug on category fitur and asset img

--- Public/scripts/redirect.js
+++ Public/scripts/redirect.js
@@ -17,40 +17,74 @@ function openCart() {
 const products = [
   {
     id: 1,
-    name: "DXRacer DRIFTING SERIES",
+    name: "Razer Blackshark",
     price: 9000000,
     category: "Headset",
-    image: "/src/img/DXRacer-DRIFTING-SERIES.png",
+    image: "/src/img/razer_blackshark.jpeg",
   },
   {
     id: 2,
-    name: "Apa kek",
+    name: "Hyper X Cloud II Pink Edition",
     price: 10000000,
     category: "Headset",
-    image: "/src/img/dxracer-drifting-oh-dh73-nc-3.jpg",
+    image:
+      "/src/img/HyperX_Cloud_II_Pink_HyperX_Headset_PK_1_15_05_2015_19_15.jpg",
   },
   {
     id: 3,
-    name: "Test",
+    name: "VortexSeries ONI R1",
     price: 800000,
     category: "Mouse",
-    image: "/src/img/OIP.jpeg"
+    image: "/src/img/ONI_R1.jpeg",
   },
-  { id: 4,
-    name: "Test",
+  {
+    id: 4,
+    name: "VXE R1 Pro Max",
     price: 800000,
     category: "Mouse",
-    image: "/src/img/OIP.jpeg" },
-  { id: 5, name: "Product 5", category: "Keyboard", image: "product5.jpg" },
-  { id: 6, name: "Product 6", category: "Keyboard", image: "product6.jpg" },
-  { id: 7, name: "Product 7", category: "Monitor", image: "product7.jpg" },
-  { id: 8, name: "Product 8", category: "Monitor", image: "product8.jpg" },
-  { id: 9, name: "Product 9", category: "Gaming Chair", image: "product9.jpg" },
+    image: "/src/img/vxe_r1.jpeg",
+  },
+  {
+    id: 5,
+    name: "Fantech Maxfit 81",
+    price: 800000,
+    category: "Keyboard",
+    image: "/src/img/maxfit_81.jpeg",
+  },
+  {
+    id: 6,
+    name: "Ajazz AK820 Pro",
+    price: 800000,
+    category: "Keyboard",
+    image: "/src/img/ajazz_ak820.jpeg",
+  },
+  {
+    id: 7,
+    name: "Zowie XL2566K",
+    price: 800000,
+    category: "Monitor",
+    image: "/src/img/ZOWIE-XL2566K-1-600x600.jpg",
+  },
+  {
+    id: 8,
+    name: "MSI G244F",
+    price: 800000,
+    category: "Monitor",
+    image: "/src/img/msi_g244f.jpeg",
+  },
+  {
+    id: 9,
+    name: "DX Racer Drifting Series",
+    price: 800000,
+    category: "Gaming Chair",
+    image: "/src/img/DXRacer-DRIFTING-SERIES.png",
+  },
   {
     id: 10,
-    name: "Product 10",
+    name: "Gaming Chair",
+    price: 800000,
     category: "Gaming Chair",
-    image: "product10.jpg",
+    image: "/src/img/OIP.jpeg",
   },
 ];
 
@@ -59,7 +93,7 @@ function displayProducts(category) {
   productSection.innerHTML = "";
 
   products.forEach((product) => {
-    if (category === "all" || product.category === category) {
+    if (product.category === category) {
       const productCard = document.createElement("div");
       productCard.classList.add("card-product");
       productCard.innerHTML = `
@@ -73,17 +107,16 @@ function displayProducts(category) {
     }
   });
 
-  const categoryLinks = document.querySelectorAll('.nav-link');
-  categoryLinks.forEach(link => {
-    link.classList.remove('active');
-    link.style.backgroundColor = '';
-    link.style.color = '#000'
+  const categoryLinks = document.querySelectorAll(".nav-link");
+  categoryLinks.forEach((link) => {
+    link.classList.remove("active");
+    link.style.backgroundColor = "";
+    link.style.color = "#000";
   });
   // event.target.classList.add('active');
-  event.target.style.backgroundColor = '#f07a3c';
-  event.target.style.color = '#fff';
+  event.target.style.backgroundColor = "#f07a3c";
+  event.target.style.color = "#fff";
 }
-
 
 const categoryLinks = document.querySelectorAll(".nav-link");
 categoryLinks.forEach((link) => {
